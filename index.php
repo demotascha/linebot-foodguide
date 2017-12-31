c90792d2e54d8c8e78780764b01dd84b925ed002
DemoHuang <demotascha@gmail.com>
2017-12-31 17:15:31 +0800

    To fix logic when keyword not found.

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ foreach ($client->parseEvents() as $event) {
 
             $json = file_get_contents('https://spreadsheets.google.com/feeds/list/15hdMRO6b2IjbYO2b6T_phn8mrAv6ysj8Bnx4auGEnzs/od6/public/values?alt=json');
             $data = json_decode($json, true);
-            $result = array();
+            $isMatchKeywords = true;
 
             foreach ($data['feed']['entry'] as $item) {
                 $keywords = explode(',', $item['gsx$keyword']['$t']);
@@ -40,44 +40,48 @@ foreach ($client->parseEvents() as $event) {
                     $image = $item['gsx$photourl']['$t'];
                     $tableTitle = $item['gsx$title']['$t'];
                 } else {
-                    $client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                                array(
-                                    'type' => 'text',
-                                    'text' => 'Hello 我今天只看得懂人名或外號, 如：黃XX / Demo',
-                                )
-                            )
-                        )
-                    );
+                    $isMatchKeywords= false;
                 }
             }
 
             switch ($message['type']) {
                 case 'text':
-                    $client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array(
-                                'type' => 'text',
-                                'text' => 'Hi ' . $message['text'] . ' 感謝您來參加婚禮',
+                    if (!$isMatchKeywords) {
+                        $client->replyMessage(array(
+                            'replyToken' => $event['replyToken'],
+                            'messages' => array(
+                                    array(
+                                        'type' => 'text',
+                                        'text' => 'Hello 我今天只看得懂人名或外號, 如：黃XX / Demo',
+                                    )
+                                )
+                            )
+                        );
+                    } else {
+                        $client->replyMessage(array(
+                            'replyToken' => $event['replyToken'],
+                            'messages' => array(
+                                array(
+                                    'type' => 'text',
+                                    'text' => 'Hi ' . $message['text'] . ' 感謝您來參加婚禮',
+                                ),
+                                array(
+                                    'type' => 'text',
+                                    'text' => '您的座位是在 ' . $tableTitle . ', 可依照下圖紅色圈圈處移動',
+                                ),
+                                array(
+                                    'type' => 'image',
+                                    'originalContentUrl' => $image,
+                                    'previewImageUrl' => $image,
+                                ),
+                                array(
+                                    'type' => 'sticker',
+                                    'packageId' => '1',
+                                    'stickerId' => '2',
+                                ),
                             ),
-                            array(
-                                'type' => 'text',
-                                'text' => '您的座位是在 ' . $tableTitle . ', 可依照下圖紅色圈圈處移動',
-                            ),
-                            array(
-                                'type' => 'image',
-                                'originalContentUrl' => $image,
-                                'previewImageUrl' => $image,
-                            ),
-                            array(
-                                'type' => 'sticker',
-                                'packageId' => '1',
-                                'stickerId' => '2',
-                            ),
-                        ),
-                    ));
+                        ));
+                    }
                     break;
                 default:
                     error_log("Unsupporeted message type: " . $message['type']);
